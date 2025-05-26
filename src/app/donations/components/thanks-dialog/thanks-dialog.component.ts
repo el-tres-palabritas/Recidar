a5b84c1d1d0bd4771a1aa159554840c86911ef61
Gareth <sebastianmv2019@gmail.com>
2025-05-25 20:05:25 -0500

    feat(donations): add thanks dialog component

---
+++ src/app/donations/components/thanks-dialog/thanks-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thanks-dialog',
+  standalone: false,
+  templateUrl: './thanks-dialog.component.html',
+  styleUrl: './thanks-dialog.component.css'
+})
+export class ThanksDialogComponent {
+
+}
